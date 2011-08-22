4575387613603f7f7400848436e85da8fa487901
Peter Halliday <pghalliday@gmail.com>
2011-08-22 22:28:46 +0200

    key store is now working correctly

--- app.js
+++ app.js
@@ -1,109 +1,123 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express');
-var io = require('socket.io');
-var KeyStore = require('./keys.js').KeyStore;
+try {
+    var express = require('express');
+    var io = require('socket.io');
+    var KeyStore = require('./keys.js').KeyStore;
 
-var sockets = {};
-var keyStore = new KeyStore(6, 500000, [
-		'a', 
-		'b', 
-		'c',
-		'd',
-		'e',
-		'f',
-		'g',
-		'h',
-		'i',
-		'j',
-		'k',
-		'l',
-		'm',
-		'n',
-		'o',
-		'p',
-		'q',
-		'r',
-		's',
-		't',
-		'u',
-		'v',
-		'w',
-		'x',
-		'y',
-		'z'
-	]);
+    var sockets = {};
 
-var app = module.exports = express.createServer()
-	, io = io.listen(app);
+    var keyStore = new KeyStore(6, 50000, [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+    ]);
 
-// Configuration
+    var app = module.exports = express.createServer()
+    ,
+    io = io.listen(app);
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+    // Configuration
+    app.configure(function() {
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'jade');
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(express.cookieParser());
+        app.use(express.session({
+            secret: 'your secret here'
+        }));
+        app.use(app.router);
+        app.use(express.static(__dirname + '/public'));
+    });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
+    app.configure('development',
+    function() {
+        app.use(express.errorHandler({
+            dumpExceptions: true,
+            showStack: true
+        }));
+    });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
+    app.configure('production',
+    function() {
+        app.use(express.errorHandler());
+    });
 
-// Routes
+    // Routes
+    app.get('/',
+    function(req, res) {
+        res.render('index', {
+            title: 'Receiver'
+        });
+    });
 
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Receiver'
-  });
-});
+    app.get('/newData',
+    function(req, res) {
+        res.render('newData', {
+            title: 'Receiver'
+        });
+    });
 
-app.get('/newData', function(req, res){
-  res.render('newData', {
-    title: 'Receiver'
-  });
-});
+    app.post('/newData',
+    function(req, res) {
+        console.log('post /newData: key: "' + req.body.key + '": newData: "' + req.body.newData + '"');
 
-app.post('/newData', function(req, res){
-  console.log('post /newData: key: "' + req.body.key + '": newData: "' + req.body.newData + '"');
+        var socket = sockets[req.body.key];
 
-  var socket = sockets[req.body.key];
+        if (socket) {
+            console.log('post /newData: socket.internalKey: "' + socket.internalKey + '"');
+            socket.emit('newData', req.body.newData);
+        }
 
-  if (socket) {
-    console.log('post /newData: socket.internalKey: "' + socket.internalKey + '"');
-    socket.emit('newData', req.body.newData);
-  }
+        res.redirect('back');
+    });
 
-  res.redirect('back');
-});
+    // io
+    io.sockets.on('connection',
+    function(socket) {
+        var newKey = keyStore.getKey();
+        sockets[newKey] = socket;
 
-// io
+        console.log('on connection: newKey: "' + newKey + '"');
 
-io.sockets.on('connection', function(socket) {
-  var newKey = keyStore.getKey();
-  sockets[newKey] = socket;
+        socket.emit('newKey', newKey);
 
-  console.log('on connection: newKey: "' + newKey + '"');
+        socket.on('disconnect',
+        function() {
+            console.log('on disconnect: newKey: "' + newKey + '"');
+            delete(sockets[newKey]);
+            keyStore.returnKey(newKey);
+        });
+    });
 
-  socket.emit('newKey', newKey);
-
-  socket.on('disconnect', function() {
-    console.log('on disconnect: newKey: "' + newKey + '"');
-    delete(sockets[newKey]);
-    keyStore.returnKey(newKey);
-  });
-});
-
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+    app.listen(3000);
+    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+} catch(e) {
+    console.log("Error:", e);
+}
--- keys.js
+++ keys.js
@@ -1,65 +1,92 @@
 // Generates unique random keys of fixed length and from a fixed set of characters
-
 exports.KeyStore = KeyStore;
 
+function KeyStoreException(summary, detail) {
+    this.summary = summary;
+    this.detail = detail;
+}
+
 function KeyStore(keyLength, maxConcurrentKeys, validCharacters) {
-	this.keyLength = keyLength;
-	this.maxConcurrentKeys = maxConcurrentKeys;
-	this.validCharacters = new Array(validCharacters);
-	
-	// initialise array of valid key ordinals, etc 
-	var keyCount = Math.pow(validCharacters.length, keyLength);
-	assert.ok(keyCount < Number.MAX_VALUE);
-	assert.ok(keyCount != Number.POSITIVE_INFINITY);
-	assert.ok(keyCount != Number.NaN);
-	assert.ok(maxConcurrentKeys <= keyCount);
-	
-	this.keyOrdinals = new Array(maxConcurrentKeys);
-	for (var i = 0; i < maxConcurrentKeys; i++) {
-		var swapIndex = Math.floor(Math.random() * (this.maxConcurrentKeys - this.currentKeyOrdinalIndex + 1)) + this.currentKeyOrdinalIndex;
-		this.keyOrdinals[i] = this.keyOrdinals[swapIndex];
-		if (this.keyOrdinals[i] is Nothing) {
-			this.keyOrdinals[i] = swapIndex;
-		}
-		this.keyOrdinals[swapIndex] = i;
-	}
-	this.keyOrdinalMap = new Object();
-	this.keyOrdinalInterval = Math.floor(keyCount / maxConcurrentKeys);
-	this.keyOrdinalOffset = 0;
-	this.currentGetKeyOrdinalIndex = 0;
-	this.currentReturnKeyOrdinalIndex = 0;
-	this.keysOutCount = 0;
+    this.keyLength = keyLength;
+    this.maxConcurrentKeys = maxConcurrentKeys;
+    this.validCharacters = validCharacters.slice(0);
+
+    // initialise array of valid key ordinals, etc
+    var keyCount = Math.pow(validCharacters.length, keyLength);
+    if (keyCount == Number.MAX_VALUE) {
+        throw new KeyStoreException('Too many keys', 'the total number of possible keys is equal to or exceeds the maximum number value');
+    }
+    if (keyCount == Number.POSITIVE_INFINITY) {
+        throw new KeyStoreException('Infinite keys', 'the total number of possible keys seems to be positive infinity... which is odd');
+    }
+    if (keyCount == Number.NaN) {
+        throw new KeyStoreException('Undefined number of keys', 'the total number of possible keys is not a number... which is odd');
+    }
+    if (maxConcurrentKeys > keyCount) {
+        throw new KeyStoreException('Too many concurrent keys', 'The maximum number of concurrent keys is greater than the total number of possible keys');
+    }
+
+    this.keyOrdinals = new Array(maxConcurrentKeys);
+	var swapIndex = 0;
+	var swapValue = 0;
+    for (var i = 0; i < maxConcurrentKeys; i++) {
+        this.keyOrdinals[i] = i;
+    }
+    for (var i = 0; i < maxConcurrentKeys; i++) {
+        swapIndex = Math.floor(Math.random() * (maxConcurrentKeys - i)) + i;
+		swapValue = this.keyOrdinals[i]
+        this.keyOrdinals[i] = this.keyOrdinals[swapIndex];
+        this.keyOrdinals[swapIndex] = swapValue;
+    }
+	console.log(this.keyOrdinals);
+    this.keyOrdinalMap = new Object();
+    this.keyOrdinalInterval = Math.floor(keyCount / maxConcurrentKeys);
+    this.keyOrdinalOffset = 0;
+    this.currentGetKeyOrdinalIndex = 0;
+    this.currentReturnKeyOrdinalIndex = 0;
+    this.keysOutCount = 0;
 };
 
 KeyStore.prototype.getKey = function() {
-	var key = Nothing;
-	if (this.keysOutCount >= maxConcurrentKeys) {
-	} else {
-		var keyNumeral = (this.keyOrdinals[this.currentGetKeyOrdinalIndex] * this.keyOrdinalInterval) + this.keyOrdinalOffset;
-		key = this.generateKey(keyNumeral);
-		this.keyOrdinalMap[key] = this.keyOrdinals[this.currentGetKeyOrdinalIndex];
-		this.currentKeyOrdinalIndex++;
-		if (this.currentGetKeyOrdinalIndex >= this.maxConcurrentKeys) {
-			this.currentGetKeyOrdinalIndex = 0;
-			this.keyOrdinalOffset++;
-			if (this.keyOrdinalOffset >= this.keyOrdinalInterval) {
-				this.keyOrdinalOffset = 0;
-			}
-		}
-		this.keysOutCount++;
-	}
-	return key;
+    var key = null;
+    if (this.keysOutCount < this.maxConcurrentKeys) {
+        var keyNumeral = (this.keyOrdinals[this.currentGetKeyOrdinalIndex] * this.keyOrdinalInterval) + this.keyOrdinalOffset;
+        key = this.generateKey(keyNumeral);
+        this.keyOrdinalMap[key] = this.keyOrdinals[this.currentGetKeyOrdinalIndex];
+        this.currentGetKeyOrdinalIndex++;
+        if (this.currentGetKeyOrdinalIndex >= this.maxConcurrentKeys) {
+            this.currentGetKeyOrdinalIndex = 0;
+            this.keyOrdinalOffset++;
+            if (this.keyOrdinalOffset >= this.keyOrdinalInterval) {
+                this.keyOrdinalOffset = 0;
+            }
+        }
+        this.keysOutCount++;
+    }
+    return key;
 };
 
 KeyStore.prototype.returnKey = function(key) {
-	if (this.keyOrdinalMap[key]) {
-		this.keyOrdinals[this.currentReturnKeyOrdinalIndex] = this.keyOrdinalMap[key];
-		delete(this.keyOrdinalMap[key]);
-		this.currentReturnKeyOrdinalIndex++;
-		this.keysOutCount--;
-	}
+    if (this.keyOrdinalMap[key]) {
+        this.keyOrdinals[this.currentReturnKeyOrdinalIndex] = this.keyOrdinalMap[key];
+        delete(this.keyOrdinalMap[key]);
+        this.currentReturnKeyOrdinalIndex++;
+        this.keysOutCount--;
+    }
 };
 
 KeyStore.prototype.generateKey = function(keyNumeral) {
-	// TODO: generate the key from characters as if it is a base encoding
-}
+    var key = "";
+    var characterIndex = 0;
+    var remainder = keyNumeral;
+    var divisor = 0;
+    var length = this.validCharacters.length;
+    for (var i = this.keyLength - 1; i > 0; i--) {
+        divisor = Math.pow(length, i);
+        characterIndex = Math.floor(remainder / divisor);
+        remainder -= (divisor * characterIndex);
+        key += this.validCharacters[characterIndex];
+    }
+    key += this.validCharacters[remainder];
+	return key;
+};
